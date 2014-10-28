6c3d4a40eecad31071f0f9718982bfa09e1b4419
Cyril Cressent <cyril@cressent.org>
2014-10-28 13:29:48 +0000

    Add an echo server to edkvs_server

--- apps/edkvs_server/lib/edkvs_server.ex
+++ apps/edkvs_server/lib/edkvs_server.ex
@@ -1,19 +1,48 @@
 defmodule EDKVSServer do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
+  @doc false
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+    import Supervisor.Spec
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(EDKVSServer.Worker, [arg1, arg2, arg3])
+      supervisor(Task.Supervisor, [[name: EDKVSServer.TaskSupervisor]]),
+      worker(Task, [EDKVSServer, :accept, [4040]])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: EDKVSServer.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  Starts accepting connections on the given `port`.
+  """
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false])
+    IO.puts "Accepting connections on port #{port}"
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    Task.Supervisor.start_child(EDKVSServer.TaskSupervisor, fn -> serve(client) end)
+    loop_acceptor(socket)
+  end
+
+  defp serve(client) do
+    client
+    |> read_line()
+    |> write_line(client)
+
+    serve(client)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
